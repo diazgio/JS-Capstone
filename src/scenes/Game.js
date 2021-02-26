28aa97ea2b3622b2cb596bdb52058f5b8d047190
Giordano Dìaz <diazgiordano@gmail.com>
2021-02-26 15:56:37 -0500

    Move to different files logic for movement

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,6 +3,7 @@ import pinkJ from '../../public/assets/pinkProta.json';
 import pinkP from '../../public/assets/pinkProta.png';
 import candyMap from '../../public/assets/sheetCandy.png';
 import candyMapJ from '../../public/assets/candymap.json';
+import PlayerController from './PlayerController';
 
 export default class Game extends Phaser.Scene {
   
@@ -10,7 +11,6 @@ export default class Game extends Phaser.Scene {
 	{
     super('Game')
     this.Hero = Phaser.Physics.Matter.Sprite;
-    this.isTouchingGround = false;
 	}
 
   init(){
@@ -24,7 +24,6 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    this.createHeroAnimations();
     const map = this.make.tilemap({ key: 'tilemap' });
     const tileSet = map.addTilesetImage('CandyWorld', 'tiles');
     const ground = map.createLayer('ground', tileSet);
@@ -37,12 +36,9 @@ export default class Game extends Phaser.Scene {
       switch(name) {
         case 'HeroSpwam': {
           this.Hero = this.matter.add.sprite(x + (width * 0.5), y, 'pinkHero')
-              .play('player-idle')
               .setFixedRotation();
-    
-          this.Hero.setOnCollide((data) => {
-            this.isTouchingGround = true;
-          });
+          
+          this.playerController = new PlayerController(this.Hero, this.cursors);
           this.cameras.main.startFollow(this.Hero);
           break
         }
@@ -52,46 +48,11 @@ export default class Game extends Phaser.Scene {
     this.matter.world.convertTilemapLayer(ground);
   }
 
-  createHeroAnimations() {
-    this.anims.create({
-      key: 'player-idle',
-      frames: [{ key: 'pinkHero',frame: 'Pink_Monster_Walk_06.png' }]
-    })
-    this.anims.create({
-      key: 'player-walk',
-      frameRate: 10,
-      frames: this.anims.generateFrameNames('pinkHero', {
-        start: 1,
-        end: 6,
-        prefix: 'Pink_Monster_Walk_0',
-        suffix: '.png'
-      }),
-      repeat: -1
-    })
-  }
+  update(t, dt){
 
-  update(){
-    const speed = 5;
-
-    if(this.cursors.left.isDown){
-      this.Hero.flipX = true;
-      this.Hero.setVelocityX(-speed);
-      this.Hero.play('player-walk', true);
-
+    if(!this.playerController) {
+      return
     }
-    else if(this.cursors.right.isDown){
-      this.Hero.flipX = false;
-      this.Hero.setVelocityX(speed);
-      this.Hero.play('player-walk', true);
-    } else {
-      this.Hero.setVelocityX(0);
-      this.Hero.play('player-idle', true);
-    }
-
-    const spaceJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
-    if(spaceJustPressed && this.isTouchingGround) {
-      this.Hero.setVelocityY(-10);
-      this.isTouchingGround = false
-    }
+    this.playerController.update(dt);
   }
 }
---
+++ src/scenes/PlayerController.js
@@ -0,0 +1,116 @@
+import Phaser from 'phaser';
+import StateMachine from '../statemachine/stateMachine';
+
+export default class PlayerController {
+  constructor(sprite, cursors) {
+    this.sprite = sprite;
+    this.cursors = cursors;
+    this.createAnimations();
+    this.stateMachine = new StateMachine(this, 'player');
+    this.stateMachine.addState('idle', {
+        onEnter: this.idleOnEnter,
+        onUpdate: this.idleOnUpdate
+    })
+        .addState('walk', {
+        onEnter: this.walkOnEnter,
+        onUpdate: this.walkOnUpdate
+    })
+        .addState('jump', {
+        onEnter: this.jumpOnEnter,
+        onUpdate: this.jumpOnUpdate
+    })
+        .setState('idle');
+    this.sprite.setOnCollide((data) => {
+      if(this.stateMachine.isCurrentState('jump')) {
+        this.stateMachine.setState('idle');
+      }
+    });
+  }
+
+  update(dt) {
+    this.stateMachine.update(dt);
+  }
+
+  idleOnEnter() {
+    this.sprite.play('player-idle');
+  }
+
+  idleOnUpdate() {
+    if (this.cursors.left.isDown || this.cursors.right.isDown) {
+        this.stateMachine.setState('walk');
+    }
+    const spaceJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
+    if (spaceJustPressed) {
+        this.stateMachine.setState('jump');
+    }
+  }
+
+  walkOnEnter() {
+    this.sprite.play('player-walk');
+  }
+
+  walkOnUpdate() {
+    const speed = 5;
+    if (this.cursors.left.isDown) {
+        this.sprite.flipX = true;
+        this.sprite.setVelocityX(-speed);
+    }
+    else if (this.cursors.right.isDown) {
+        this.sprite.flipX = false;
+        this.sprite.setVelocityX(speed);
+    }
+    else {
+        this.sprite.setVelocityX(0);
+        this.stateMachine.setState('idle');
+    }
+    const spaceJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.space);
+    if (spaceJustPressed) {
+        this.stateMachine.setState('jump');
+    }
+  }
+
+  jumpOnEnter() {
+    this.sprite.setVelocityY(-10);
+    this.sprite.play('player-jump');
+  }
+
+  jumpOnUpdate() {
+    const speed = 5;
+    if (this.cursors.left.isDown) {
+        this.sprite.flipX = true;
+        this.sprite.setVelocityX(-speed);
+    }
+    else if (this.cursors.right.isDown) {
+        this.sprite.flipX = false;
+        this.sprite.setVelocityX(speed);
+    }
+  }
+
+  createAnimations() {
+    this.sprite.anims.create({
+        key: 'player-idle',
+        frames: [{ key: 'pinkHero', frame: 'Pink_Monster_Walk_06.png' }]
+    });
+    this.sprite.anims.create({
+        key: 'player-walk',
+        frameRate: 10,
+        frames: this.sprite.anims.generateFrameNames('pinkHero', {
+            start: 1,
+            end: 6,
+            prefix: 'Pink_Monster_Walk_0',
+            suffix: '.png'
+        }),
+        repeat: -1
+    });
+    this.sprite.anims.create({
+        key: 'player-jump',
+        frameRate: 5,
+        frames: this.sprite.anims.generateFrameNames('pinkHero', {
+            start: 1,
+            end: 8,
+            prefix: 'Pink_Monster_Jump_0',
+            suffix: '.png'
+        })
+    });
+  }
+}
